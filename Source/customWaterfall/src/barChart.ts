94ebf0993baa0d800ad7117432e765956c6b0437
Sauget Charles-Henri <chsauget@scop-it.com>
2018-09-09 13:59:10 +0200

    First WaterFall
    First waterfall version with constant datas

--- Source/customWaterfall/src/barChart.ts
+++ Source/customWaterfall/src/barChart.ts
@@ -9,6 +9,7 @@ module powerbi.extensibility.visual {
     interface BarChartViewModel {
         dataPoints: BarChartDataPoint[];
         dataMax: number;
+        dataMin: number;
     };
 
     /**
@@ -21,6 +22,9 @@ module powerbi.extensibility.visual {
     interface BarChartDataPoint {
         value: number;
         category: string;
+        start: number;
+        end: number;
+        class: string;
     };
 
     export class BarChart implements IVisual {
@@ -29,9 +33,20 @@ module powerbi.extensibility.visual {
         private barChartContainer: d3.Selection<SVGElement>;
         private barContainer: d3.Selection<SVGElement>;
         private bars: d3.Selection<SVGElement>;
-
+        private xAxis: d3.Selection<SVGElement>;
+        private yAxis: d3.Selection<SVGElement>;
+        
         static Config = {
             xScalePadding: 0.1,
+            solidOpacity: 1,
+            transparentOpacity: 0.4,
+            margins: {
+                top: 10,
+                right: 0,
+                bottom: 25,
+                left: 45,
+            },
+            xAxisFontMultiplier: 0.04,
         };
 
         /**
@@ -47,9 +62,15 @@ module powerbi.extensibility.visual {
             let svg = this.svg = d3.select(options.element)
                 .append('svg')
                 .classed('barChart', true);
-
+            
             this.barContainer = svg.append('g')
                 .classed('barContainer', true);
+            this.xAxis = this.svg
+                .append('g')
+                .classed('xAxis', true);
+            this.yAxis = this.svg
+                .append('g')
+                .classed('yAxis', true);
         }
 
         /**
@@ -63,31 +84,58 @@ module powerbi.extensibility.visual {
         public update(options: VisualUpdateOptions) {
             let testData: BarChartDataPoint[] = [
                 {
-                    value: 10,
-                    category: 'a'
-                },
-                {
-                    value: 20,
-                    category: 'b'
-                },
-                {
-                    value: 1,
-                    category: 'c'
-                },
-                {
                     value: 100,
-                    category: 'd'
+                    category: 'Total',
+                    start: 0,
+                    end: 0,
+                    class: ''
+                }
+                ,
+                {
+                    value: 60,
+                    category: 'effect1',
+                    start: 0,
+                    end: 0,
+                    class: ''
+                }
+                ,
+                {
+                    value: -20,
+                    category: 'effect2',
+                    start: 0,
+                    end: 0,
+                    class: ''
                 },
                 {
-                    value: 500,
-                    category: 'e'
-                }];
+                    value: -10,
+                    category: 'd',
+                    start: 0,
+                    end: 0,
+                    class: ''
+                },
+                {
+                    value: 30,
+                    category: 'e',
+                    start: 0,
+                    end: 0,
+                    class: ''
+                }
+                ,
+                {
+                    value: 160,
+                    category: 'Total G',
+                    start: 0,
+                    end: 0,
+                    class: ''
+                }
+            ];
 
             let viewModel: BarChartViewModel = {
                 dataPoints: testData,
-                dataMax: d3.max(testData.map((dataPoint) => dataPoint.value))
+                dataMax: d3.max(testData.map((dataPoint) => dataPoint.value)),
+                dataMin : 0//d3.min(testData.map((dataPoint) => dataPoint.value))
             };
-
+            //Retrieve the visualisation size from PowerBI
             let width = options.viewport.width;
             let height = options.viewport.height;
 
@@ -96,24 +144,69 @@ module powerbi.extensibility.visual {
                 height: height
             });
 
-            let yScale = d3.scale.linear()
-                .domain([0, viewModel.dataMax])
-                .range([height, 0]);
+            let margins = BarChart.Config.margins;
+            height -= (margins.bottom + margins.top);
+            width -= margins.left;
 
+            this.xAxis.style({
+                "font-size": d3.min([height, width]) * BarChart.Config.xAxisFontMultiplier
+            });
+            this.yAxis.style({
+                "font-size": d3.min([height, width]) * BarChart.Config.xAxisFontMultiplier
+            });
+
+            let yScale = d3.scale.linear()
+                .domain([viewModel.dataMin, viewModel.dataMax])
+                .range([height, 0]);
             let xScale = d3.scale.ordinal()
                 .domain(viewModel.dataPoints.map(d => d.category))
                 .rangeRoundBands([0, width], BarChart.Config.xScalePadding);
 
+            let xAxis = d3.svg.axis()
+                .scale(xScale)
+                .orient('bottom');
+
+            this.xAxis.attr('transform', 'translate('+margins.left+', ' + (height + margins.top)  + ')')
+                .call(xAxis);  
+
+            let yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient('left');
+
+            this.yAxis.attr('transform', 'translate(51, '+margins.top+')')
+                .call(yAxis);               
+            
+
+            // Pre calculate start and end of each bar
+            var cumulative = 0;
+            for (var i = 0; i < viewModel.dataPoints.length; i++) {
+                
+                viewModel.dataPoints[i].start = cumulative;
+                cumulative += viewModel.dataPoints[i].value;
+                viewModel.dataPoints[i].end = cumulative;
+                
+                viewModel.dataPoints[i].class = ( viewModel.dataPoints[i].value >= 0 ) ? 'positive' : 'negative'
+                if(i==viewModel.dataPoints.length - 1)
+                {
+                    viewModel.dataPoints[i].start = 0;
+                    viewModel.dataPoints[i].end = viewModel.dataPoints[i].value;
+                }
+            }
+
+            //console.log('Visual update' , options);
+            //debugger;
             let bars = this.barContainer.selectAll('.bar').data(viewModel.dataPoints);
             bars.enter()
                 .append('rect')
                 .classed('bar', true);
             bars.attr({
                 width: xScale.rangeBand(),
-                height: d => height - yScale(d.value),
-                y: d => yScale(d.value),
-                x: d => xScale(d.category)
+                height: d => height - yScale(Math.abs( d.start - d.end)),
+                y: d => yScale(Math.max(d.start, d.end) ),
+                x: d => xScale(d.category),
+                transform: 'translate('+margins.left+', '+margins.top+')'
             });
+
 
             bars.exit()
                 .remove();
